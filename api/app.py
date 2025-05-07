acfcd649c0529e4b07178929d2bbaeab5038ccde
Vineeth <56399644+vineeth729@users.noreply.github.com>
2025-05-07 12:28:44 +0530

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,9 +1,27 @@
 from flask import Flask
+import psycopg2
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from the API!"
+    try:
+        conn = psycopg2.connect(
+            host=os.environ.get("DB_HOST"),
+            port=os.environ.get("DB_PORT"),
+            user=os.environ.get("DB_USER"),
+            password=os.environ.get("DB_PASS"),
+            dbname="postgres"
+        )
+        cur = conn.cursor()
+        cur.execute("SELECT version();")
+        db_version = cur.fetchone()
+        cur.close()
+        conn.close()
+        return f"Hello from the API! Successfully connected to DB version: {db_version[0]}..."
+    except Exception as e:
+        return f"Database connection failed: {str(e)}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
